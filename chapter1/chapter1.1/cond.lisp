fcb46d17588476157ae2e893f32189c138dc18a4
jason chen <jasonChen.cl1989@gmail.com>
2018-04-07 18:35:31 +0800

    [feature] init & finish 1.1

---
+++ chapter1/chapter1.1/cond.lisp
@@ -0,0 +1,20 @@
+;; (define (abs x) 
+;;   (cond ((< x 0) (- x))
+;;         ((= x 0) 0)
+;;         ((> x 0) x)))
+;; (abs -5)
+
+;; (define (abs x)
+;;         (cond ((< x 0) (- x))
+;;         (else x)))
+;; (abs -10)
+
+;; (define (abs x) 
+;;   (if (< x 0) 
+;;       (- x) 
+;;       x))
+;; (abs -20)
+
+(define (>= x y) (or (> x y) (= x y)))
+(>= 10 5)
+(>= 5 10)
